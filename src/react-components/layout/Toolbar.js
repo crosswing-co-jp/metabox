01e679cc553f46b489155e36cf153803455572d0
Keisuke Shirato <shirato.engineer@gmail.com>
2023-12-20 21:44:05 +0900

    スマホでのメニューバーレイアウト調整

--- src/react-components/layout/Toolbar.js
+++ src/react-components/layout/Toolbar.js
@@ -4,16 +4,16 @@ import classNames from "classnames";
 import styles from "./Toolbar.scss";
 import styleUtils from "../styles/style-utils.scss";
 
-export function Toolbar({ className, left, center, right, ...rest }) {
+export function Toolbar({ className, left, center, right, entered, ...rest }) {
   return (
-    <div className={classNames(styles.toolbar, className)} {...rest}>
+    <div className={classNames(styles.toolbar, className, entered && styles.customToolbar)} {...rest}>
       <div className={classNames(styles.starWrapper)}>
         <div className={classNames(styles.smallStar)} />
         <div className={classNames(styles.mediumStars)} />
         <div className={classNames(styles.bigStars)} />
       </div>
       <div className={classNames(styles.content, styles.leftContent, styleUtils.showLg)}>{left}</div>
-      <div className={classNames(styles.content, styles.centerContent)}>{center}</div>
+      <div className={classNames(styles.content, entered && styles.centerContent)}>{center}</div>
       <div className={classNames(styles.content, styles.rightContent, styleUtils.showLg)}>{right}</div>
     </div>
   );
@@ -24,6 +24,7 @@ Toolbar.propTypes = {
   left: PropTypes.node,
   center: PropTypes.node,
   right: PropTypes.node,
+  entered: PropTypes.bool,
   hideLeft: PropTypes.string,
   hideRight: PropTypes.string
 };
